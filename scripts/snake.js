9756d055ad27285d473035bd1ef6c4b618295c31
MagicSaros <magicsaros@gmail.com>
2017-11-16 15:11:14 +0300

    Fix direction issues

--- scripts/snake.js
+++ scripts/snake.js
@@ -36,8 +36,9 @@ class Snake {
         this.size = size;        
         this.color = color;
         this.speed = speed;
-        this.xSpeed = speed;
+        this.xSpeed = 0;
         this.ySpeed = 0;
+        this.direction = null;
         this.body = [];
         for (let i = 0; i < this.length; i++) {
             this.body.push(new Segment(this.x - i * this.size, this.y, this.size, this.color));
@@ -54,25 +55,42 @@ class Snake {
         this.body.unshift(new Segment(this.x, this.y, this.size, this.color));
         this.body.pop();
     }
-
+    
     changeDirection(direction) {
-        switch(direction) {
-            case 'up':
+        if (!this.checkDirectionCollision(direction)) {
+            this.direction = direction;
+            switch(this.direction) {
+                case 'up':
                 this.xSpeed = 0;
                 this.ySpeed = this.speed;
                 break;
-            case 'down':
+                case 'down':
                 this.xSpeed = 0;
                 this.ySpeed = -this.speed;
                 break;
-            case 'left':
+                case 'left':
                 this.xSpeed = -this.speed;
                 this.ySpeed = 0;
                 break;
-            case 'right':
+                case 'right':
                 this.xSpeed = this.speed;
                 this.ySpeed = 0;
                 break;
+            }
+        }
+    }
+
+    checkDirectionCollision(newDirection) {
+        if (this.direction === 'up' && newDirection === 'down') {
+            return true;
+        } else if (this.direction === 'down' && newDirection === 'up') {
+            return true;
+        } else if (this.direction === 'left' && newDirection === 'right') {
+            return true;
+        } else if (this.direction === 'right' && newDirection === 'left') {
+            return true;
+        } else {
+            return false;
         }
     }
 
@@ -119,118 +137,101 @@ class Food extends Segment {
     }
 }
 
-let snake = null;
+let snake1 = null;
 let snake2 = null;
 let size = 20;
-let snakeStartX = fieldWidth / 2;
-let snakeStartY = fieldHeight / 2;
+let snake1StartX = fieldWidth / 2;
+let snake1StartY = fieldHeight / 2;
 let snake2StartX = fieldWidth / 2 + size * 2;
 let snake2StartY = fieldHeight / 2 + size * 2;
-let snakeLength = 5;
-let snakeColor = 'Red';
+let snake1Length = 5;
+let snake2Length = 5;
+let snake1Color = 'Red';
 let snake2Color = 'Blue';
-let snakeSpeed = 1;
+let snake1Speed = 1;
+let snake2Speed = 1;
+let snake1Direction = 'right';
+let snake2Direction = 'right';
 let food = null;
-let foodX = 0;
-let foodY = 0;
+let foodX = null;
+let foodY = null;
 let foodColor = 'Green';
-let score = 0;
+let score1 = null;
+let score2 = null;
 let gameID = null;
 let gameRunning = false;
-let key = 37;
-let prevKey = 39;
-let key2 = 65;
-let prevKey2 = 68;
 
 const keyActions = {
     37: 'left',
     38: 'up',
     39: 'right',
     40: 'down', 
-    87: 'up',
     65: 'left',
-    83: 'down',
-    68: 'right'
-};
-
-let checkOppositeKey = (key, prevKey) => {
-    if (Math.abs(key - prevKey) == 2) {
-        return true;
-    } else {
-        return false;
-    }
+    87: 'up',
+    68: 'right',
+    83: 'down'
 };
 
 $('body').keydown(event => {
-    key = event.keyCode;
-    if (!checkOppositeKey(key, prevKey)) {
+    let key = event.keyCode;
         switch(key) {
             case 37:
             case 38:
             case 39:
             case 40:
-            snake.changeDirection(keyActions[key]);
+            snake1.changeDirection(keyActions[key]);
+            break;
+            case 65: // A
+            case 87: // W
+            case 68: // D
+            case 83: // S
+            snake2.changeDirection(keyActions[key]);
             break;
         };
-        prevKey = key;
-    }
-});
-
-$('body').keydown(event => {
-    key2 = event.keyCode;
-    if (!checkOppositeKey(key2, prevKey2)) {
-        switch(key2) {
-            case 87: //w
-            case 65: //a
-            case 83: //s
-            case 68: //d
-            snake2.changeDirection(keyActions[key2]);
-            break;
-        };
-        prevKey2 = key2;
-    }
-});
+}); 
 
 $('.button-start').click(() => {
-    snake = new Snake(snakeStartX, snakeStartY, snakeLength, size, snakeColor, snakeSpeed);
-    snake2 = new Snake(snake2StartX, snake2StartY, snakeLength, size, snake2Color, snakeSpeed);
-    snake.draw();
+    snake1 = new Snake(snake1StartX, snake1StartY, snake1Length, size, snake1Color, snake1Speed, snake1Direction);
+    snake2 = new Snake(snake2StartX, snake2StartY, snake2Length, size, snake2Color, snake2Speed, snake2Direction);
+    snake1.draw();
     snake2.draw();
+    snake1.changeDirection(snake1Direction);
+    snake2.changeDirection(snake2Direction);
     
     do {
         foodY = Math.floor(Math.random() * (Math.floor(fieldHeight / size) - 2) + 1) * size;
         foodX = Math.floor(Math.random() * (Math.floor(fieldWidth / size) - 2) + 1) * size;
         food = new Food(foodX, foodY, size, foodColor);
-    } while (snake.checkFoodCollision(food) || snake2.checkFoodCollision(food));
+    } while (snake1.checkFoodCollision(food) || snake2.checkFoodCollision(food));
     food.draw();
 
     if (!gameRunning) {
-        score = 0;
-        $('.score-points').html(score);
+        score1 = 0;
+        $('.score-points').html(score1);
         $('.gameover').addClass('hidden');
         gameRunning = true;
         gameID = setInterval(() => {
             ctx.clearRect(0, 0, fieldWidth, fieldHeight);
-            snake.move();
+            snake1.move();
             snake2.move();
-            if ((snake.checkSelfCollision() || snake.checkWallCollision()) || (snake2.checkSelfCollision() || snake2.checkWallCollision())) {
+            if ((snake1.checkSelfCollision() || snake1.checkWallCollision()) || (snake2.checkSelfCollision() || snake2.checkWallCollision())) {
                 clearInterval(gameID);
                 gameRunning = false;
                 $('.gameover').removeClass('hidden');
             } 
-            if (snake.checkFood(food)) {
-                snake.eatFood(food);
-                score++;
-                $('.score-points').html(score);
+            if (snake1.checkFood(food)) {
+                snake1.eatFood(food);
+                score1++;
+                $('.score-points').html(score1);
                 do {
                     foodY = Math.floor(Math.random() * (Math.floor(fieldHeight / size) - 2) + 1) * size;
                     foodX = Math.floor(Math.random() * (Math.floor(fieldWidth / size) - 2) + 1) * size;
                     food = new Food(foodX, foodY, size, foodColor);
-                } while (snake.checkFoodCollision(food));
+                } while (snake1.checkFoodCollision(food));
             } else if (snake2.checkFood(food)) {
                 snake2.eatFood(food);
-                score++;
-                $('.score-points').html(score);
+                score1++;
+                $('.score-points').html(score1);
                 do {
                     foodY = Math.floor(Math.random() * (Math.floor(fieldHeight / size) - 2) + 1) * size;
                     foodX = Math.floor(Math.random() * (Math.floor(fieldWidth / size) - 2) + 1) * size;
@@ -238,7 +239,7 @@ $('.button-start').click(() => {
                 } while (snake2.checkFoodCollision(food));
             }
             food.draw();
-            snake.draw();
+            snake1.draw();
             snake2.draw();
         }, 100);
     }
